1711e4bfb07a29b0392330185262b97a086d564d
Phil Hanna <ph1204@gmail.com>
2022-06-07 22:05:16 -0400

    Added docstring to split_date() function

--- longevity/longevity.py
+++ longevity/longevity.py
@@ -2,6 +2,8 @@ from datetime import date
 
 
 def split_date(dob: date):
+    """ Extracts date fields and formats them the way the
+    cgi program expects them."""
     monthofbirth = f"{dob.month - 1}"
     dayofbirth = f"{dob.day:02d}"
     yearofbirth = f"{dob.year}"
