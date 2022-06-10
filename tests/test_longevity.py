c6c53f8bd138fee0dad8a9477e33c59fe24bb276
Phil Hanna <ph1204@gmail.com>
2022-06-10 09:08:14 -0400

    Added test for Keith Richards

--- tests/test_longevity.py
+++ tests/test_longevity.py
@@ -7,9 +7,10 @@ from longevity import Longevity
 class TestLongevity(TestCase):
 
     def test_longevity(self):
+        # Keith Richards
         sex = "m"
-        dob = date.fromisoformat("1953-12-04")
+        dob = date.fromisoformat("1943-12-18")
         long = Longevity(sex, dob)
-        self.assertEqual(68.5, long.current_age)
-        self.assertEqual(16.5, long.additional_years)
-        self.assertEqual(85, long.total_years)
+        self.assertEqual(941/12, long.current_age)
+        self.assertEqual(9.6, long.additional_years)
+        self.assertEqual(88.1, long.total_years)
