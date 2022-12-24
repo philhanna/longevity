fdf54ea4ebf0fd69e2b2967a5afab0d9e0631e82
Phil Hanna <ph1204@gmail.com>
2022-12-24 16:51:18 -0500

    Bumped version number to 1.2.0

--- setup.py
+++ setup.py
@@ -2,17 +2,13 @@ from setuptools import setup
 
 setup(
     name='longevity',
-    version='1.0.0',
+    version='1.2.0',
     packages=['tests', 'longevity'],
     install_requires=[
-        'beautifulsoup4==4.11.1',
-        'bs4==0.0.1',
-        'certifi==2022.9.24',
-        'charset-normalizer==2.1.1',
-        'idna==3.4',
-        'requests==2.28.1',
-        'soupsieve==2.3.2.post1',
-        'urllib3==1.26.12',
+        'beautifulsoup4',
+        'bs4',
+        'pytest',
+        'requests',
     ],
     url='https://github.com/philhanna/longevity',
     license='MIT',
