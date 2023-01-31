8ea05df88d03f1d892ffa549a6e7d00065b3a054
Phil Hanna <ph1204@gmail.com>
2023-01-31 00:22:39 -0500

    Added examples

---
+++ example_get_test.go
@@ -0,0 +1,31 @@
+package longevity_test
+
+import (
+	"fmt"
+	"longevity"
+	"os"
+	"time"
+)
+
+// This example illustrates how to use the longevity package in a standalone mainline
+func ExampleGet() {
+	const ISO_FORMAT = "2006-01-02"
+	sex := "m"
+	dob, _ := time.Parse(ISO_FORMAT, "1943-12-18")
+	// Invoke the requester
+	resp, err := longevity.Get(sex, dob)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		return
+	}
+	currentAge := resp.CurrentAge
+	additionalYears := resp.AdditionalYears
+	totalYears := resp.TotalYears
+	deathDate := resp.DeathDate
+	deathYear, deathMonth, deathDay := deathDate.Date()
+
+	fmt.Printf("current age      = %.1f\n", currentAge)
+	fmt.Printf("additional years = %.1f\n", additionalYears)
+	fmt.Printf("total years      = %.1f\n", totalYears)
+	fmt.Printf("expiration date  = %4d-%02d-%02d\n", deathYear, deathMonth, deathDay)
+}
